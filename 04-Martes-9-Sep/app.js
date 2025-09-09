d6ded5f0389ecae1f2aa2b03e724289f5a487f94
auriza-10 <auriza@centro.edu.mx>
2025-09-09 14:32:58 -0600

    Agregó: las primeras líneas verticales.

--- 04-Martes-9-Sep/app.js
+++ 04-Martes-9-Sep/app.js
@@ -7,3 +7,20 @@ canvas.height = window.innerHeight;
 
 // Definir contexto 2D
 const ctx = canvas.getContext('2d');
+
+ctx.beginPath();
+ctx.strokeStyle= "#005b86ff";
+ctx.lineWidth= 8;
+ctx.moveTo(0, 1200);
+ctx.lineTo(0, 0);
+ctx.moveTo(20, 1200);
+ctx.lineTo(20, 0);
+ctx.moveTo(40, 1200);
+ctx.lineTo(40, 0);
+ctx.moveTo(60, 1200);
+ctx.lineTo(60, 0);
+ctx.stroke();
+ctx.closePath();
+
+
+
