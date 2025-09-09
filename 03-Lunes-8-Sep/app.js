313d15edbe68191c4b7c1821a96a069cead606b2
auriza-10 <auriza@centro.edu.mx>
2025-09-08 19:38:24 -0600

    Agregó: Una nueva carpeta con sus correspondientes html, css y js.

---
+++ 03-Lunes-8-Sep/app.js
@@ -0,0 +1,10 @@
+console.log("Jardín Github. Ejercicio 03: Autorretrato abstracto");
+
+const canvas = document.getElementById("lienzo");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// Definir contexto 2D
+const ctx = canvas.getContext('2d');
+
