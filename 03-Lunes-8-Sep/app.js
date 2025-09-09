2cf316ebc70aaa7da68a864e5ffc8372de4fdd2b
auriza-10 <auriza@centro.edu.mx>
2025-09-08 20:57:36 -0600

    Agregó: dos cuadrados en el js para simular el rostro y cuello.

--- 03-Lunes-8-Sep/app.js
+++ 03-Lunes-8-Sep/app.js
@@ -8,3 +8,14 @@ canvas.height = window.innerHeight;
 // Definir contexto 2D
 const ctx = canvas.getContext('2d');
 
+ctx.beginPath();
+ctx.fillStyle = "#d0a466ff";
+ctx.rect(600, 200, 200, 250);
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.fillStyle = "#d0a466ff";
+ctx.rect(650, 300, 100, 200);
+ctx.fill();
+ctx.closePath();
