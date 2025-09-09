a5f508d4dc6de0f0f88eaa7ebd94e21cbda1a207
auriza-10 <auriza@centro.edu.mx>
2025-09-09 11:30:24 -0600

    Agregó: una nueva carpeta de actividad 4 con su html, css y js.

---
+++ 04-Martes-9-Sep/app.js
@@ -0,0 +1,9 @@
+console.log("Jardín Github. Ejercicio 04: Composición de líneas");
+
+const canvas = document.getElementById("lienzo");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// Definir contexto 2D
+const ctx = canvas.getContext('2d');
