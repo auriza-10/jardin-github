d52e2d683851699d92732f19c2ab7fe866dab0d9
auriza-10 <auriza@centro.edu.mx>
2025-09-12 22:25:22 -0600

    Agregó: el código en app.js para animar un torus knot que esté rotando.

--- 07-Viernes-12-Sep/app.js
+++ 07-Viernes-12-Sep/app.js
@@ -1,14 +1,32 @@
-console.log("Jardín Github. Ejercicio 07: Creación de un árbol");
+console.log("Jardín Github. Ejercicio 07: Objeto 3D que cambia de color cada que la ventana cambia de tamaño");
 console.log(THREE);
 
 const canvas = document.getElementById("lienzo");
+
 canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.height = window.innerHeight; 
 
 const scene = new THREE.Scene();
-
+const renderer = new THREE.WebGLRenderer({
+  antialias: true,
+  canvas: canvas,
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
 const camera = new THREE.PerspectiveCamera(45, canvas.width / canvas.height, 0.1, 1000);
-camera.position.z = 60;
 
-const renderer = new THREE.WebGLRenderer({ canvas: canvas });
-renderer.setSize(canvas.width, canvas.height);
+const geometry = new THREE.TorusKnotGeometry();
+
+const material = new THREE.MeshBasicMaterial({ color: "#ff0000" }); 
+
+const torusKnot = new THREE.Mesh(geometry, material);
+scene.add(torusKnot);
+torusKnot.position.z = -5;
+torusKnot.rotation.x = 45;
+
+function animate() {
+    requestAnimationFrame(animate);
+    torusKnot.rotation.x += 0.01;
+    torusKnot.rotation.y += 0.01;
+    renderer.render(scene, camera);
+}
+animate();
