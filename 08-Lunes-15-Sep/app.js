5d6724d1e394ec77c33291c6916219bb6629f3e9
auriza-10 <auriza@centro.edu.mx>
2025-09-15 16:25:52 -0600

    Agregó: una nueva carpeta para la actividad 8 y su html, css y js.

---
+++ 08-Lunes-15-Sep/app.js
@@ -0,0 +1,11 @@
+console.log("Jardín Github. Ejercicio 08: Boomerang");
+
+const canvas = document.getElementById("lienzo");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const ctx = canvas.getContext('2d');
+
+
+
