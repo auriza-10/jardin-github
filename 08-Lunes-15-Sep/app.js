6253c19429dc3d2de024d71666e25e36f3c339d1
auriza-10 <auriza@centro.edu.mx>
2025-09-15 19:14:21 -0600

    Agregó: gsap.To para lograr que mi boomerang se moviera de un extremo a otro en el canvas.

--- 08-Lunes-15-Sep/app.js
+++ 08-Lunes-15-Sep/app.js
@@ -6,3 +6,38 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const ctx = canvas.getContext('2d');
+
+window.addEventListener("mousedown", function () {
+  gsap.to(".boomerang", {
+    duration: 2,
+    x: window.innerWidth / 2, 
+    y: -200,                  
+    ease: "power2.inOut",
+    onComplete: () => {
+      gsap.to(".boomerang", {
+        duration: 2,
+        x: window.innerWidth - 150, 
+        y: 0,
+        ease: "power2.inOut",
+        onComplete: () => {
+          gsap.to(".boomerang", {
+            duration: 2,
+            x: window.innerWidth / 2,
+            y: -200,
+            ease: "power2.inOut",
+            onComplete: () => {
+              gsap.to(".boomerang", {
+                duration: 2,
+                x: 0,
+                y: 0,
+                ease: "power2.inOut",
+                });
+            }
+            });
+        }
+        });
+    }
+    });
+});
+
+
