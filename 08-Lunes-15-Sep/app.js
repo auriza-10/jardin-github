2742497a94a0e2b8ecad5daafdbdf85479c3e4d0
auriza-10 <auriza@centro.edu.mx>
2025-09-15 19:15:49 -0600

    Agregó: un gsap.To para hacer rotar el boomerang durante su transcurso.

--- 08-Lunes-15-Sep/app.js
+++ 08-Lunes-15-Sep/app.js
@@ -9,6 +9,12 @@ const ctx = canvas.getContext('2d');
 
 window.addEventListener("mousedown", function () {
   gsap.to(".boomerang", {
+    rotation: 360,
+    duration: 1,
+    repeat: -1,
+    ease: "linear"
+  });
+  gsap.to(".boomerang", {
     duration: 2,
     x: window.innerWidth / 2, 
     y: -200,                  
@@ -31,13 +37,12 @@ window.addEventListener("mousedown", function () {
                 x: 0,
                 y: 0,
                 ease: "power2.inOut",
-                });
+              });
             }
-            });
+          });
         }
-        });
+      });
     }
-    });
+  });
 });
-
 
