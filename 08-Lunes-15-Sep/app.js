e7861948883aaf363e30817e8c771155e88ea3f8
auriza-10 <auriza@centro.edu.mx>
2025-09-15 18:26:20 -0600

    Agregó: un for en css para crear el boomerang con dos elipses.

--- 08-Lunes-15-Sep/app.js
+++ 08-Lunes-15-Sep/app.js
@@ -7,5 +7,22 @@ canvas.height = window.innerHeight;
 
 const ctx = canvas.getContext('2d');
 
+ctx.fillStyle = 'black';
+ctx.lineWidth = 15;
+
+for (let i = 0; i < 2; i++) {
+  let dir = i === 0 ? -1 : 1;
+
+  let x = 400 + dir * 50; // dir = dirección
+  let y = 300 + 60;
+
+  ctx.beginPath();
+  ctx.ellipse(x, y, 70, 25, dir * Math.PI / 5, 0, 3 * Math.PI);
+  ctx.fill();
+}
+
+
+
+
 
 
