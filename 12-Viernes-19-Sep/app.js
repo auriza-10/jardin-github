dd751873ca678b8822cf7569b05ec7ff7cd7d79a
auriza-10 <auriza@centro.edu.mx>
2025-09-19 23:27:36 -0600

    Añadió: los ajustes para el botón y modifiqué el archivo js añadiendo la figura con una textura.

--- 12-Viernes-19-Sep/app.js
+++ 12-Viernes-19-Sep/app.js
@@ -2,14 +2,45 @@ console.log("Jardín Github. Ejercicio 12: Objeto 3D que al hacer click en el bo
 console.log(THREE);
 
 const canvas = document.getElementById("lienzo");
-
 canvas.width = window.innerWidth;
-canvas.height = window.innerHeight; 
+canvas.height = window.innerHeight;
 
 const scene = new THREE.Scene();
-const renderer = new THREE.WebGLRenderer({
-  antialias: true,
-  canvas: canvas,
-});
-renderer.setSize(window.innerWidth, window.innerHeight);
 const camera = new THREE.PerspectiveCamera(45, canvas.width / canvas.height, 0.1, 1000);
+const geometry = new THREE.TorusKnotGeometry();
+
+const material = new THREE.MeshNormalMaterial({ flatShading: true,});
+const textureLoader = new THREE.TextureLoader();
+var matcapMaterial;
+var mesh;
+var matcapMap = textureLoader.load(
+   './texturas/plateado.png',
+   function (texture) {
+       matcapMaterial = new THREE.MeshMatcapMaterial( { matcap: texture } );
+       mesh = new THREE.Mesh( geometry, matcapMaterial );
+       scene.add(mesh);
+       mesh.position.z= -8;
+       animate();
+   },
+
+   undefined,
+   function (error) { console.error("Algo salió mal con la textura", error);}
+);
+
+
+const renderer = new THREE.WebGLRenderer({ canvas : canvas});
+renderer.setSize(canvas.width, canvas.height);
+
+renderer.render(scene, camera);
+
+
+function animate() {
+   requestAnimationFrame(animate);
+
+   mesh.rotation.x += 0.01;
+   mesh.rotation.y += 0.01;
+
+   renderer.render(scene, camera);
+}
+
+
